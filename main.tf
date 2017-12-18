31e37a4f0870524b44a202bc0cc0749d732e0ad9
Sharath <sharath.av@ymedialabs.com>
2017-12-18 17:53:20 +0530

    making instance vpc optional

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
 module "my-ec2" {
-  source = "git::ssh://git@bitbucket.org/ymedialabs/terraform-modules.git//modules//ec2?ref=v0.2"
+  source = "git::ssh://git@bitbucket.org/ymedialabs/terraform-modules.git//modules//ec2?ref=v0.21"
+  instance_name             = "example"
+  aws_region                = "us-west-2"
+  ami_id                    = "ami-7707a10f"
+  disk_size                 = "8"
+  instance_type             = "t2.small"
+  key_name                  = "yml-internal-key"
+  iam_instance_profile      = "EC2CodeDeployRole"
+  vpc_id                    = "vpc-b1267cd7"
+  subnet_id                 = "${var.adobe-aem-poc_ec2_subnet_id}"
+  inbound_sgs_count         = 0
+  allow_inbound_ips         = ["${var.adobe-aem-poc_ec2_ssh_port}, 	0.0.0.0/0",
+                                "80, 0.0.0.0/0",
+                                "4503, 0.0.0.0/0",
+                                "${var.adobe-aem-poc_ec2_tcp_port},  0.0.0.0/0",]
+
+  additional_tags {
+  company = "yml"
+  for = "Prashant, Guru"
+  project = "adobe-aem-poc"
+}
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -8,7 +8,7 @@ variable "subnet_id"              {}
 variable "iam_instance_profile"   {default=""}
 variable "disk_size"              {}
 variable "instance_name"          {}
-variable "vpc_id"                 {}
+variable "vpc_id"                 {default=""}
 variable "allow_inbound_sgs"      {type="list", default=[]}
 variable "inbound_sgs_count"      {default=0}
 variable "allow_inbound_ips"      {type="list", default=[]}
