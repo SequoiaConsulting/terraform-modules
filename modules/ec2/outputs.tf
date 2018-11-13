0c54882b8cb306217a53ef7b093d53c65fc94eb0
Sharath <sharath.av@ymedialabs.com>
2018-11-13 13:46:16 +0530

    Adding private IP as output for EC2 instance

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -7,3 +7,6 @@ output "security_groups" {
 output "public_ips" {
   value = "${aws_instance.ec2-instance.*.public_ip}"
 }
+output "private_ips" {
+  value = "${aws_instance.ec2-instance.*.private_ip}"
+}
