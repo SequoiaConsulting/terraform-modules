728d6ce5560fb54de0ff1e11503037b8cc6a39e3
Vikrant_Garg <vikrant.garg@ymedialabs.com>
2018-12-26 17:54:31 +0530

    Adding alb suffix and tg suffix in output vars

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -13,6 +13,14 @@ output "alb_dns_name" {
 output "alb_zone_id" {
     value = "${aws_alb.alb.zone_id}"
 }
+
+output "alb_arn_suffix" {
+    value = "${aws_alb.alb.arn_suffix}"
+}
+
+output "alb_tg_arn_suffix" {
+    value = "${aws_alb_target_group.alb-tg.*.arn_suffix}"
+}
 /*
 output "alb_http_listener_arn" {
 value = "${aws_alb_listener.alb-http-listener.arn}"
