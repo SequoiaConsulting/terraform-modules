f91adddacaf2b4bfed05750535570d29a1a788a3
Sharath AV <sharath.av@sequioa.com>
2019-09-14 09:18:45 +0530

    Updating ASG module code for terraform v0.12

--- modules/asg/asg.tf
+++ modules/asg/asg.tf
@@ -1,11 +1,25 @@
 resource "aws_launch_configuration" "lc" {
-  name                        = "${var.asg_name}-lc-${var.image_id}"
-  image_id                    = "${var.image_id}"
-  instance_type               = "${var.instance_type}"
-  iam_instance_profile        = "${var.iam_instance_profile}"
-  security_groups             = ["${var.security_groups}"]
-  root_block_device           = "${var.root_block_device}"
-  key_name                    = "${var.key_name}"
+  name                 = "${var.asg_name}-lc-${var.image_id}"
+  image_id             = var.image_id
+  instance_type        = var.instance_type
+  iam_instance_profile = var.iam_instance_profile
+  security_groups      = var.security_groups
+  dynamic "root_block_device" {
+    for_each = var.root_block_device
+    content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
+
+      delete_on_termination = lookup(root_block_device.value, "delete_on_termination", null)
+      encrypted             = lookup(root_block_device.value, "encrypted", null)
+      iops                  = lookup(root_block_device.value, "iops", null)
+      volume_size           = lookup(root_block_device.value, "volume_size", null)
+      volume_type           = lookup(root_block_device.value, "volume_type", null)
+    }
+  }
+  key_name = var.key_name
   lifecycle {
     create_before_destroy = true
   }
@@ -13,79 +27,97 @@ resource "aws_launch_configuration" "lc" {
 
 resource "aws_autoscaling_group" "asg" {
   name                 = "${var.asg_name}-asg"
-  launch_configuration = "${aws_launch_configuration.lc.name}"
-  min_size             = "${var.min_size}"
-  max_size             = "${var.max_size}"
-  vpc_zone_identifier  = ["${var.vpc_zone_identifier}"]
-  target_group_arns    = ["${var.target_group_arns}"]
-  health_check_type    = "${var.health_check_type}"
-  tags = ["${concat(
-    list(
-      map("key", "Name", "value", "${var.asg_name}-(autoscaled)", "propagate_at_launch", true),
-      map("key", "managed-by", "value", "terraform", "propagate_at_launch", true)
+  launch_configuration = aws_launch_configuration.lc.name
+  min_size             = var.min_size
+  max_size             = var.max_size
+  vpc_zone_identifier  = var.vpc_zone_identifier
+  target_group_arns    = var.target_group_arns
+  health_check_type    = var.health_check_type
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibilty in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  tags = [
+    concat(
+      [
+        {
+          "key"                 = "Name"
+          "value"               = "${var.asg_name}-(autoscaled)"
+          "propagate_at_launch" = true
+        },
+        {
+          "key"                 = "managed-by"
+          "value"               = "terraform"
+          "propagate_at_launch" = true
+        },
+      ],
+      var.additional_tags,
     ),
-    var.additional_tags)
-  }"]
+  ]
   lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_autoscaling_policy" "scale-up" {
-    name = "${var.asg_name}-scale-up"
-    scaling_adjustment = 1
-    adjustment_type = "ChangeInCapacity"
-    cooldown = 300
-    autoscaling_group_name = "${aws_autoscaling_group.asg.name}"
+  name                   = "${var.asg_name}-scale-up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.asg.name
 }
 
 resource "aws_autoscaling_policy" "scale-down" {
-    name = "${var.asg_name}-scale-down"
-    scaling_adjustment = -1
-    adjustment_type = "ChangeInCapacity"
-    cooldown = 300
-    autoscaling_group_name = "${aws_autoscaling_group.asg.name}"
+  name                   = "${var.asg_name}-scale-down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.asg.name
 }
 
 resource "aws_cloudwatch_metric_alarm" "high-cpu" {
-    alarm_name = "${var.asg_name}-high-cpu-alarm"
-    comparison_operator = "GreaterThanOrEqualToThreshold"
-    evaluation_periods = "3"
-    metric_name = "CPUUtilization"
-    namespace = "AWS/EC2"
-    period = "60"
-    statistic = "Average"
-    threshold = "80"
+  alarm_name          = "${var.asg_name}-high-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "3"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "80"
 
-    dimensions {
-      AutoScalingGroupName = "${aws_autoscaling_group.asg.name}"
-    }
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
 
-    alarm_description = "Autoscaling group High CPU utilization"
-    alarm_actions = ["${aws_autoscaling_policy.scale-up.arn}"]
+  alarm_description = "Autoscaling group High CPU utilization"
+  alarm_actions     = [aws_autoscaling_policy.scale-up.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpualarm-down" {
-    alarm_name = "${var.asg_name}-low-cpu-alarm"
-    comparison_operator = "LessThanOrEqualToThreshold"
-    evaluation_periods = "5"
-    metric_name = "CPUUtilization"
-    namespace = "AWS/EC2"
-    period = "60"
-    statistic = "Average"
-    threshold = "30"
+  alarm_name          = "${var.asg_name}-low-cpu-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "5"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "30"
 
-    dimensions {
-      AutoScalingGroupName = "${aws_autoscaling_group.asg.name}"
-    }
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
 
-    alarm_description = "Autoscaling group Low CPU utilization"
-    alarm_actions = ["${aws_autoscaling_policy.scale-down.arn}"]
+  alarm_description = "Autoscaling group Low CPU utilization"
+  alarm_actions     = [aws_autoscaling_policy.scale-down.arn]
 }
 
 resource "aws_autoscaling_notification" "asg-notification" {
   group_names = [
-    "${aws_autoscaling_group.asg.name}"
+    aws_autoscaling_group.asg.name,
   ]
 
   notifications = [
@@ -94,11 +126,11 @@ resource "aws_autoscaling_notification" "asg-notification" {
     "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
   ]
 
-  topic_arn = "${var.sns_topic_arn}"
+  topic_arn = var.sns_topic_arn
 }
 
 resource "aws_cloudwatch_metric_alarm" "low-healthy-host-count" {
-  count               = "${var.allow_healthy_host_policy}"
+  count               = var.allow_healthy_host_policy
   alarm_name          = "${var.asg_name}-Low-HealthyHostCount"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "1"
@@ -106,22 +138,22 @@ resource "aws_cloudwatch_metric_alarm" "low-healthy-host-count" {
   namespace           = "AWS/ApplicationELB"
   period              = "60"
   statistic           = "Average"
-  threshold           = "${var.min_size - 1}"
+  threshold           = var.min_size - 1
 
-  dimensions {
-    LoadBalancer = "${var.alb_arn_suffix}"
-    TargetGroup  = "${var.alb_tg_arn_suffix}"
-}
+  dimensions = {
+    LoadBalancer = var.alb_arn_suffix
+    TargetGroup  = var.alb_tg_arn_suffix
+  }
 
-  alarm_description  = "Trigger an alert when target Group has low healthy hosts count"
-  alarm_actions      = ["${aws_autoscaling_policy.HealthyHostCount-Scaling-Up-policy.arn}"]
+  alarm_description = "Trigger an alert when target Group has low healthy hosts count"
+  alarm_actions     = [aws_autoscaling_policy.HealthyHostCount-Scaling-Up-policy[0].arn]
 }
 
 resource "aws_autoscaling_policy" "HealthyHostCount-Scaling-Up-policy" {
-  count                  = "${var.allow_healthy_host_policy}"
+  count                  = var.allow_healthy_host_policy
   name                   = "${var.asg_name}-scale-up-HealthyHostCount"
   scaling_adjustment     = 1
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 300
-  autoscaling_group_name = "${aws_autoscaling_group.asg.name}"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
 }
--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -1,39 +1,68 @@
-variable "asg_name"             {}
-variable "image_id"             {}
-variable "instance_type"        {}
-variable "iam_instance_profile" {}
-variable "security_groups"      {type = "list"}
-variable "key_name"             {}
-variable "min_size"             {}
-variable "max_size"             {}
-variable "vpc_zone_identifier"  {type = "list"}
-variable "target_group_arns"    {type = "list"}
-variable "sns_topic_arn"        {}
+variable "asg_name" {
+}
+
+variable "image_id" {
+}
+
+variable "instance_type" {
+}
+
+variable "iam_instance_profile" {
+}
+
+variable "security_groups" {
+  type = list(string)
+}
+
+variable "key_name" {
+}
+
+variable "min_size" {
+}
+
+variable "max_size" {
+}
+
+variable "vpc_zone_identifier" {
+  type = list(string)
+}
+
+variable "target_group_arns" {
+  type = list(string)
+}
+
+variable "sns_topic_arn" {
+}
+
 variable "alb_arn_suffix" {
   default = ""
 }
+
 variable "alb_tg_arn_suffix" {
   default = ""
 }
+
 variable "additional_tags" {
-  type = "list"
+  type = list(string)
   default = [
     {
-      key = "managed-by"
-      value = "terraform"
+      key                 = "managed-by"
+      value               = "terraform"
       propagate_at_launch = true
-    }
+    },
   ]
 }
+
 variable "root_block_device" {
   description = "Customize details about the root block device of the instance"
-  type        = "list"
-  default     = [
+  type        = list(string)
+  default = [
     {
       volume_type = "gp2"
-    }
+    },
   ]
 }
+
 variable "health_check_type" {
   default = "ELB"
 }
