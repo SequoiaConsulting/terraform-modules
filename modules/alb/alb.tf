1b12ebe152d592fcf958a5d8d64258db8e56ec29
Sharath AV <sharath.av@sequioa.com>
2019-11-07 18:45:51 +0530

    Fix typo on cidr-block

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "alb-sg" {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    ipv6_cidr_cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
